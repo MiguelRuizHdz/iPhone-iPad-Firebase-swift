662a191289cdcf6a16deb27b6f331da5bc2e9b5b
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-10-11 12:17:04 -0500

    Instalar firebase con package manager

--- IphoneIpadFirebase/AppDelegate.swift
+++ IphoneIpadFirebase/AppDelegate.swift
@@ -5,4 +5,13 @@
 //  Created by Miguel Angel Ruiz on 11/10/22.
 //
 
-import Foundation
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
