de6811858164c24aa2918bce8d701d9d8fe66a63
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-10-11 11:34:31 -0500

    Initial Commit

---
+++ IphoneIpadFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IphoneIpadFirebase
+//
+//  Created by Miguel Angel Ruiz on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IphoneIpadFirebase/IphoneIpadFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneIpadFirebaseApp.swift
+//  IphoneIpadFirebase
+//
+//  Created by Miguel Angel Ruiz on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct IphoneIpadFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
