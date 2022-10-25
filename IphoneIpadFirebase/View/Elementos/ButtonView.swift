418b40466515b9c2df74241305248a733982e220
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-10-25 10:43:01 -0500

    Menu solo para iPhone

--- IphoneIpadFirebase/View/Elementos/ButtonView.swift
+++ IphoneIpadFirebase/View/Elementos/ButtonView.swift
@@ -10,11 +10,17 @@ import SwiftUI
 struct ButtonView: View {
     @Binding var index : String
     @Binding var menu : Bool
+    var device = UIDevice.current.userInterfaceIdiom
     var title : String
     
     var body: some View {
         Button(action: {
-            withAnimation{index = title}
+            withAnimation{
+                index = title
+                if device == .phone{
+                    menu.toggle()
+                }
+            }
         }){
             Text(title)
                 .font(.title)
