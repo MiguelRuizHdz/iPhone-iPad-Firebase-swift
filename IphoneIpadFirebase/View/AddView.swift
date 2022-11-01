81ae9a140f5c93799e3563c9c69358b15d1bcfe6
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-01 12:55:19 -0600

    Registrar usuario desde la app

---
+++ IphoneIpadFirebase/View/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  IphoneIpadFirebase
+//
+//  Created by Miguel Angel Ruiz on 01/11/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
