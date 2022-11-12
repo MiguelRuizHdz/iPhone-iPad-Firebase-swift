f1709ad50f17759d14606ace10d6d5b9c6f40762
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-12 17:58:38 -0600

    Eliminar registros

--- IphoneIpadFirebase/View/Elementos/CardView.swift
+++ IphoneIpadFirebase/View/Elementos/CardView.swift
@@ -12,6 +12,11 @@ struct CardView: View {
     var titulo : String
     var portada : String
     
+    var index  : FirebaseModel
+    var plataforma : String
+    
+    @StateObject var datos = FirebaseViewModel()
+    
     var body: some View {
         VStack(spacing: 20){
             ImagenFirebase(imageUrl: portada)
@@ -19,6 +24,15 @@ struct CardView: View {
                 .font(.title)
                 .bold()
                 .foregroundColor(.black)
+            Button(action:{
+                datos.delete(index: index, plataforma: plataforma)
+            }){
+                Text("Eliminar")
+                    .foregroundColor(.red)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 25)
+                    .background(Capsule().stroke(Color.red))
+            }
         }.padding()
         .background(Color.white)
         .cornerRadius(20)
